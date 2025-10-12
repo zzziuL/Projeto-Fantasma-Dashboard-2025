bc5c4aa8dedb6f1a8a8358d9c9a82c5282dd3b2e
zzziuL <ludura9@gmail.com>
2025-10-12 20:26:17 -0300

    entrega parcial 1

--- rdocs/entrega_1.R
+++ rdocs/entrega_1.R
@@ -22,3 +22,56 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+#Carregando as pastas
+
+relatoriovendas <- read_xlsx("relatorio_old_town_road.xlsx",
+                   sheet = "relatorio_vendas")
+infosvendas <- read_xlsx("relatorio_old_town_road.xlsx",
+                   sheet = "infos_vendas")
+produtos <- read_xlsx("relatorio_old_town_road.xlsx",
+                      sheet = "infos_produtos")
+
+##ENTREGA1
+
+#Renomeando os items Sale e Item
+infosvendas <- infosvendas %>%
+  rename(SaleID = Sal3ID)
+produtos <- produtos %>%
+  rename(ItemID = Ite3ID)
+
+#Juntando os itens
+dados <- inner_join(relatoriovendas, infosvendas, by = "SaleID")
+dados1 <- inner_join(dados, produtos, by = "ItemID")
+
+#Criando coluna Ano
+dados1 <- dados1 %>%
+  mutate(Ano = year(Date))
+
+#Filtrando e criando a receita
+dados1 <- dados1 %>%
+  filter(Ano >= 1880 & Ano <= 1889) %>%
+  mutate(Receita = Quantity * UnityPrice)
+
+#Calculando a média
+media <- dados1 %>%
+  group_by(Ano) %>%
+  summarise(media_anual = mean(Receita, na.rm = TRUE))
+
+#Transformando em Real
+media$media_anual <- media$media_anual * 5.31
+
+#Corrigindo a ordem para o grafico
+media <- media %>%
+  arrange(Ano)
+
+#Criando graficos
+linha <- ggplot(media) +
+  aes(x=as.factor(Ano), y=media_anual, group=1) +
+  geom_line(size=1,colour="#A11D21") + geom_point(colour="#A11D21",
+                                                  size=2) +
+  labs(x="Ano", y="Media Anual") +
+  theme_estat()
+
+#Quadro
+print_quadro_resumo(media, media_anual)
